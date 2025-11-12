d9533d93857f62fb2e0f3c2bce725adb31b88a00
Eduol4 <eduardodrevessu@gmail.com>
2025-11-12 11:30:15 -0300

    barra de pesquisa funcionando

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 10.10.2024 -> Estrutura e estilo base;
 
 11.11.2025 -> Mudanças na estilização, estrutura e o início do funcionamento;
+
+12.11.2025 -> Implementado o funcionamento da barra de pesquisa; Implementada a primeira opção desbloqueável;
--- main/comandos.js
+++ main/comandos.js
@@ -1,11 +1,43 @@
-// SESSÃO DE AJUDA
+// VARIÁVEIS
 const botaoajuda = document.getElementById("btnAjuda");
 const secaoAjuda = document.getElementById("secaoAjuda");
 
-botaoajuda.addEventListener("click", () => {
+const campoComando = document.getElementById("digiteComando");
+
+const btnSinosauropteryx = document.getElementById("btnSinosauropteryx");
+
+// SEÇÃO DE AJUDA
+// 1. BOTÃO DE "AJUDA"
+function alternarAjuda() {
     if (secaoAjuda.style.display === "none" || secaoAjuda.style.display === "") {
         secaoAjuda.style.display = "block";
     } else {
         secaoAjuda.style.display = "none";
+    }
+}
+btnAjuda.addEventListener("click", alternarAjuda);
+
+// SEÇÃO DE ANIMAIS DESBLOQUEADOS
+if(localStorage.getItem("sinosauropteryxDesbloqueado") === "true") {
+    btnSinosauropteryx.style.display = "block";
+}
+
+// SEÇÃO DE COMANDOS
+campoComando.addEventListener("keydown", (event) => {
+    if(event.key === "Enter") {
+        const comando = (campoComando.value || '').trim().toLowerCase();
+        if (comando === "ajuda") {
+            secaoAjuda.style.display = "block";
+
+            if(!localStorage.getItem("sinosauropteryxDesbloqueado")) {
+                console.log("Novo animal descoberto: Sinosauropteryx");
+                alert("Você desbloqueou um novo animal: Sinosauropteryx!");
+                btnSinosauropteryx.style.display = "block";
+                localStorage.setItem("sinosauropteryxDesbloqueado", "true");
+            }
+        } else if(comando === "sair") {
+            secaoAjuda.style.display = "none";
+        }
+        campoComando.value = "";
     }
 });
